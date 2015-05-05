46d8d3e16cd34e9a34741e81ddd519a7e124988c
Brandon Kelley <14bmkelley@gmail.com>
2015-05-04 23:00:26 -0700

    Added second activity and "glue" working.

--- java/com/brandonmkelley/fibonacci/InputActivity.java
+++ java/com/brandonmkelley/fibonacci/InputActivity.java
@@ -1,7 +1,12 @@
 package com.brandonmkelley.fibonacci;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnKeyListener;
+import android.widget.EditText;
 
 public class InputActivity extends Activity {
 
@@ -10,6 +15,27 @@ public class InputActivity extends Activity {
 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_input);
+
+		final EditText input = (EditText) findViewById(R.id.inputActivity_input);
+		input.setOnKeyListener(new OnKeyListener() {
+
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+				if (keyCode == KeyEvent.KEYCODE_ENTER) {
+
+					Intent intent = new Intent(getBaseContext(), OutputActivity.class);
+					startActivity(intent);
+
+					return true;
+
+				}
+
+				return false;
+
+			}
+
+		});
 
 	}
 
---
+++ java/com/brandonmkelley/fibonacci/OutputActivity.java
@@ -0,0 +1,14 @@
+package com.brandonmkelley.fibonacci;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OutputActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_output);
+	}
+
+}
